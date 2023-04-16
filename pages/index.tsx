597ea94bcc9a689b44033e3a96e17c0e54718812
Sebastian Korfmann <sebastian@korfmann.net>
2023-04-17 00:50:33 +0200

    Fix lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const LeadTextSection = () => (
     <section className="mx-auto max-w-7xl">
       <p className="text-2xl text-justify leading-relaxed tracking-wide text-gray-600">
         Discover the AI revolution with Machine Minds! Connect with fellow AI enthusiasts,
-        visionaries, and industry experts in our inclusive meetup group. Dive into AI's
+        visionaries, and industry experts in our inclusive meetup group. Dive into AI{"'"}s
         transformative impact on tech, society, and businesses of all sizes. Join now, make
         friends, gain insights, and shape a smarter future together!
       </p>
