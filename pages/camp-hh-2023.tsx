9b9364eed8366e7393a7467aa600191d5da63559
Sebastian Korfmann <sebastian@korfmann.net>
2023-05-15 09:55:59 +0200

    Add open graph info

--- pages/camp-hh-2023.tsx
+++ pages/camp-hh-2023.tsx
@@ -6,6 +6,12 @@ import React, { useState, useEffect } from 'react';
 import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline';
 import ReactModal from 'react-modal';
 
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
+
+
 interface PopupProps {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
@@ -287,6 +293,7 @@ const Sessions = function SessionIdeas() {
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
           <figure className="col-span-2 hidden sm:block sm:rounded-2xl sm:bg-white sm:shadow-lg sm:ring-1 sm:ring-gray-900/5 xl:col-start-2 xl:row-end-1">
+
             <blockquote className="p-12 text-xl font-semibold leading-8 tracking-tight text-gray-900">
               <p>{`“${featuredSessionIdea.body}”`}</p>
             </blockquote>
@@ -737,10 +744,21 @@ const TicketCTA =  function TicketCTA() {
 
 
 const MachineMindsCamp: NextPage = () => {
+  const logoUrl = `${publicRuntimeConfig.publicUrl}/og-camp.png`;
   return(
     <>
       <Head>
         <title>Machine Minds Camp</title>
+        <meta
+          name="description"
+          content="Das erste Machine Minds Camp findet am Freitag, 2. Juni in der Hamburger Hafencity bei der New Work SE im New Work Harbour statt."
+        />
+        <meta property="og:title" content="Machine Minds Camp Hamburg 2023" />
+        <meta
+          property="og:description"
+          content="Das erste Machine Minds Camp findet am Freitag, 2. Juni in der Hamburger Hafencity bei der New Work SE im New Work Harbour statt."
+        />
+        <meta property="og:image" content={logoUrl} />
       </Head>
       <div>
         <Hero />
