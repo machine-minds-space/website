d6556a46cdb36002c50ac7738618e57e1cb2c9e2
Sebastian Korfmann <sebastian@korfmann.net>
2023-05-16 14:58:40 +0200

    Add tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
+        <script src="https://cdn.usefathom.com/script.js" data-site="ZGWVOOID" defer></script>
       </Head>
       <body>
         <Main />
